dcccdd0ba0c113bbbf6bb86962672650a18e6b82
JC <j.cfrane@gmail.com>
2021-09-24 00:25:02 +0800

    Trying out secrets

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
-  console.log('MEOW')
   const bucket = core.getInput('bucket')
-  console.log('Bucket', bucket)
-  core.setOutput('message', 'sucessfully executed')
+  const accessKey = core.getInput('accessKey')
+
+  core.setOutput('message',  {
+    bucket, accessKey
+  })
 
 } catch (error) {
-  console.log('meow')
   core.setFailed(error.message)
 }
