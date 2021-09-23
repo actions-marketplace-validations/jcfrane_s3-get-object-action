4ae80ba62b76895f4725c13cadd68369a2283eda
JC <j.cfrane@gmail.com>
2021-09-24 00:50:22 +0800

    Trying out s3 getObject

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const aws = require('aws/sdk')
+const fs = require('fs')
 
 try {
   const bucket = core.getInput('bucket')
+  const region = core.getInput('region')
   const accessKey = core.getInput('aws-access-key')
   const secretKey = core.getInput('aws-secret-key')
 
-  core.setOutput('message',  {
-    bucket, accessKey, secretKey
-  })
+  AWS.config.update({ 
+    region,
+    accessKeyId: accessKey,
+    secretKey: secretKey 
+  });
+
+  const s3 = new AWS.S3({apiVersion: '2006-03-01'})
+  const object = s3.getObject('build_artifacts/prod.yml')
+  fs.writeFileSync('./prod.yml', object);
+
+  core.setOutput('message', 'Done')
 
 } catch (error) {
   core.setFailed(error.message)
