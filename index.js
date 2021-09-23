15bf708aab23f496ae9542bf7daf220d4a5265c7
JC <j.cfrane@gmail.com>
2021-09-24 00:34:09 +0800

    fix reading from secrets

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const github = require('@actions/github')
 
 try {
   const bucket = core.getInput('bucket')
-  const accessKey = core.getInput('accessKey')
+  const accessKey = core.getInput('aws-access-key')
+  const secretKey = core.getInput('aws-secret-key')
 
   core.setOutput('message',  {
-    bucket, accessKey
+    bucket, accessKey, secretKey
   })
 
 } catch (error) {
